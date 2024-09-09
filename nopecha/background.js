b2432de94f2ef95f791b88548e29d5ba16657c90
mcnutthelen8 <mcnutthelen8@gmail.com>
2024-09-09 22:41:57 +0530

    Delete nopecha directory

--- nopecha/background.js
+++
@@ -1,8 +0,0 @@
-// Example to handle messages, if you need it for more complex scenarios
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'clickPlayButton') {
-      // Possibly interact with content scripts or other background tasks
-      console.log("Message received to click play button");
-    }
-  });
-  

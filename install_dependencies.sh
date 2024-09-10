19f83ab2c3a74a6e5ff0d43d8363874353c4091e
mcnutthelen8 <mcnutthelen8@gmail.com>
2024-09-10 17:25:17 +0530

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -32,13 +32,13 @@ sudo apt-get update
 xrandr --output VNC-0 --mode 1920x1080
 sudo apt install unzip
 wget "https://clients2.googleusercontent.com/crx/blobs/AVsOOGhXta8-UhRWEUcB2CckwYwwFI6WMiZ8b-CxyBlFaRSv-8wsJGQEG3lL_ILVoKOmDEowQmnAnB2cwNoa3h3lbPBUsTP376PSoi8nM_fjkQSkoiDkoTx0-5M5j9WIdrlaAMZSmuUvAQiNCBMqU-HcXaveN1nJlDMohw/DCLJFNHBJPILFPMIMIPCIJGAALCABFHD_0_0_1_0.crx" -O mysterium.crx
-wget "https://clients2.googleusercontent.com/crx/blobs/AVsOOGj770VyacuboorG752M_MoplMXpBqv27gaGClAoAPeW92og_XcYErayPTq6isteFYXVGH-1-3b2N0R1CqL_2W1il76ss8wS5mm3w7ZLiHXE93yBpXDSXVZoznOZBPEAxlKa5bvVqk7IzuoqSHwWklCswZPR2Hsg/HLKENNDEDNHFKEKHGCDICDFDDNKALMDM_1_13_0_0.crx" -O cookie.crx
-wget "https://clients2.googleusercontent.com/crx/blobs/AVsOOGjQdui7oibybsnwnagZ_gz6_Zu_CSxKFbJpSbPGQSjDvf5FQBwps0BbXAAISoLmOfERqxVo_sGsBxUIAqX3N52hc5TZ4WXLPVNRnq6hp2Mjrb6FAMZSmuVHB-8YWXsgMMqOWnMm_jDpP5G-qw/MEOJNMFHJKAHLFCECPDCDGJCLCILMAIJ_1_0_5_0.crx" -O fingerprint.crx
-wget "https://clients2.googleusercontent.com/crx/blobs/AVsOOGhiVwWZyTfRn1a8tz5BGGIwjYMKxyrBvu_w00IgX7TjhJ-wiNAFbK_Use5iSp-upY6IKcIOi6jHcokx9kWzZmvxh4NmcKVVezMq81m9LqFP5NIuaTLugxHck9UHYvQAxlKa5V1Z0fOQGbAyy2lS51SFYNnB33uU/DKNLFMJAANFBLGFDFEBHIJALFMHMJJJO_0_4_12_0.crx" -O nopecha.crx
+#wget "https://clients2.googleusercontent.com/crx/blobs/AVsOOGj770VyacuboorG752M_MoplMXpBqv27gaGClAoAPeW92og_XcYErayPTq6isteFYXVGH-1-3b2N0R1CqL_2W1il76ss8wS5mm3w7ZLiHXE93yBpXDSXVZoznOZBPEAxlKa5bvVqk7IzuoqSHwWklCswZPR2Hsg/HLKENNDEDNHFKEKHGCDICDFDDNKALMDM_1_13_0_0.crx" -O cookie.crx
+#wget "https://clients2.googleusercontent.com/crx/blobs/AVsOOGjQdui7oibybsnwnagZ_gz6_Zu_CSxKFbJpSbPGQSjDvf5FQBwps0BbXAAISoLmOfERqxVo_sGsBxUIAqX3N52hc5TZ4WXLPVNRnq6hp2Mjrb6FAMZSmuVHB-8YWXsgMMqOWnMm_jDpP5G-qw/MEOJNMFHJKAHLFCECPDCDGJCLCILMAIJ_1_0_5_0.crx" -O fingerprint.crx
+#wget "https://clients2.googleusercontent.com/crx/blobs/AVsOOGhiVwWZyTfRn1a8tz5BGGIwjYMKxyrBvu_w00IgX7TjhJ-wiNAFbK_Use5iSp-upY6IKcIOi6jHcokx9kWzZmvxh4NmcKVVezMq81m9LqFP5NIuaTLugxHck9UHYvQAxlKa5V1Z0fOQGbAyy2lS51SFYNnB33uU/DKNLFMJAANFBLGFDFEBHIJALFMHMJJJO_0_4_12_0.crx" -O nopecha.crx
 
 unzip mysterium.crx -d /root/Desktop/MFV6/mysterium
-unzip cookie.crx -d /root/Desktop/MFV6/cookie
-unzip fingerprint.crx -d /root/Desktop/MFV6/fingerprint
-unzip nopecha.crx -d /root/Desktop/MFV6/nopecha
+#unzip cookie.crx -d /root/Desktop/MFV6/cookie
+#unzip fingerprint.crx -d /root/Desktop/MFV6/fingerprint
+#unzip nopecha.crx -d /root/Desktop/MFV6/nopecha
 
 echo "Installation completed successfully!"

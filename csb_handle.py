0d6ac25002514c4b192bf367c929d2850114b896
mcnutthelen8 <mcnutthelen8@gmail.com>
2024-09-13 15:41:41 +0530

    Create csb_handle.py

---
+++ csb_handle.py
@@ -0,0 +1,240 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException
+from urllib.parse import urlparse, parse_qs
+import time
+import re
+import requests
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import random
+import requests
+from requests.exceptions import RequestException
+from seleniumbase import SB
+from seleniumbase import Driver
+import subprocess
+import pyautogui
+from datetime import datetime
+import pytz
+import datetime
+import cv2
+import numpy as np
+from PIL import Image
+from pymongo import MongoClient
+from paddleocr import PaddleOCR
+import Levenshtein
+import json
+
+chrome_binary_path = '/opt/google/chrome/google-chrome'
+chrome_user_data_dir = '/root/.config/google-chrome/'
+fresh = True
+
+CSB_id = 'captaingranda'
+def pin_extensions():
+    try:
+        x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/extension_icon.png", region=(1700, 30, 300, 300), confidence=0.9)
+        pyautogui.click(x, y)
+        print("extension_icon Button Found")
+
+        for i in range(1,400):
+            time.sleep(1)
+            pyautogui.moveTo(1700, 30)
+            try:
+                x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/pin.png", region=(1588, 170, 400, 600), confidence=0.9)
+                pyautogui.click(x, y)
+                pyautogui.moveTo(1700, 30)
+                print("pin Button Found")
+                time.sleep(1)    
+            except pyautogui.ImageNotFoundException:
+                print("No pin Button.")
+            try:
+                x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/all_pinned.png", region=(1588, 170, 400, 600), confidence=0.99)
+                pyautogui.moveTo(1700, 40)
+                print("all_pinned Button Found")
+                return True   
+            except pyautogui.ImageNotFoundException:
+                print("No all_pinned Button.")
+    except pyautogui.ImageNotFoundException:
+        print("No extension_icon Button.")
+        return False
+
+def install_extensions(extension_name):
+    #on chrome://extensions/
+
+    for i in range(1,4):
+        try:
+            x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/dev_off.png", region=(1700, 95, 300, 300), confidence=0.9)
+            pyautogui.click(x, y)
+            print("Developer Button Found")
+        except pyautogui.ImageNotFoundException:
+            print("No Developer Button.")
+        time.sleep(2)
+        try:
+            x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/load_unpack.png", region=(2, 100, 400, 400), confidence=0.90)
+            pyautogui.click(x, y)
+            print("load_unpack Button Found")
+            time.sleep(2)
+            for i in range(1,100):
+                try:
+                    x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/mfv6_unselect.png", region=(388, 260, 300, 300), confidence=0.60)
+                    pyautogui.click(x, y)
+                    print("mfv6_unselect Button Found")
+                        
+                except pyautogui.ImageNotFoundException:
+                    print("No mfv6_unselect Button.")
+                try:
+                    x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/mfv6_select.png", region=(388, 260, 300, 300), confidence=0.90)
+                    pyautogui.click(x, y)
+                    print("mfv6_select Button Found")
+                    #####Select Your Extension
+                    extension_path = f"/root/Desktop/MFV6/images/{extension_name}.png" 
+                    try:
+                        x, y = pyautogui.locateCenterOnScreen(extension_path, region=(545, 200, 500, 500), confidence=0.9)
+                        pyautogui.click(x, y)
+                        print(f"{extension_path} folder Found")
+                        time.sleep(2)
+                        pyautogui.click(1467, 123)
+                        print(f"{extension_path} folder Installed Complete")
+                        return True
+                    
+                    except pyautogui.ImageNotFoundException:
+                        print(f"No {extension_path} folder.")
+                except pyautogui.ImageNotFoundException:
+                    print("No mfv6_select Button.")
+                time.sleep(1)
+                print(f'Waiting Seconds:{i}')
+
+        except pyautogui.ImageNotFoundException:
+            print("No load_unpack Button.")
+    return None
+
+
+def are_extensions_exist():
+    try:
+        x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/extension_icon.png", region=(1700, 30, 300, 300), confidence=0.9)
+        #pyautogui.click(x, y)
+        print("extension_icon Button Found")
+        return True
+
+    except pyautogui.ImageNotFoundException:
+        print("No extension_icon Button.")
+        return False
+
+def codesandbox_login(codesandbox_id):
+    pyautogui.click(1613, 137)
+    time.sleep(2)
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('t')
+    pyautogui.keyUp('ctrl')
+    time.sleep(2)
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('l')
+    pyautogui.keyUp('ctrl')
+    time.sleep(2)
+    pyautogui.typewrite('https://codesandbox.io/')
+    pyautogui.press('enter')
+    time.sleep(5)
+    for i in range(1,100):
+        time.sleep(1)
+        try:
+            x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/cookie_icon.png", region=(1625, 43, 400, 300), confidence=0.99)
+            pyautogui.click(x, y)
+            print("cookie_icon Found")
+            time.sleep(3)
+            try:
+                x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/all_site.png", region=(1300, 212, 600, 300), confidence=0.99)
+                pyautogui.click(x, y)
+                print("all_site Found")
+            except pyautogui.ImageNotFoundException:
+                print("No all_site .")
+            try:
+                x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/import_icon.png", region=(1300, 212, 900, 900), confidence=0.99)
+                pyautogui.click(x, y)
+                print("import_icon Found")
+                time.sleep(3)
+                for i in range(1,50):
+                    url = f'https://raw.githubusercontent.com/mcnutthelen8/MFV6/main/CBS_logins/{codesandbox_id}.json'
+                    response = requests.get(url)
+                    if response.status_code == 200:
+                        text_content = response.text
+                    else:
+                        print(f"Failed to retrieve the content. Status code: {response.status_code}")
+                        text_content = None
+                    if text_content:
+                        pyautogui.click(1385, 310)
+                        time.sleep(1)
+                        pyautogui.typewrite(text_content)
+                        time.sleep(5)
+                        try:
+                            x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/import_icon.png", region=(1300, 212, 900, 900), confidence=0.99)
+                            pyautogui.click(x, y)
+                            print("import_icon Found")
+                            
+                            time.sleep(3)
+                            pyautogui.click(113, 100)
+                            pyautogui.press('f5')
+                            time.sleep(3)
+                            return True
+                        
+                        except pyautogui.ImageNotFoundException:
+                            print(f"No import_icon .{i}")
+                    time.sleep(1)
+
+
+            except pyautogui.ImageNotFoundException:
+                print("No import_icon .")
+
+        except pyautogui.ImageNotFoundException:
+            print("No cookie_icon .")
+
+        try:
+            x, y = pyautogui.locateCenterOnScreen("/root/Desktop/MFV6/images/allow_button.png", region=(1080, 247, 400, 300), confidence=0.99)
+            pyautogui.click(x, y)
+            print("allow_button Found")
+                    
+        except pyautogui.ImageNotFoundException:
+            print("No allow_button .")
+
+def are_codesand_logged(driver):
+    sb1.open('https://codesandbox.io/dashboard/recent')
+    time.sleep(2)
+    for i in range(1,9999):
+        try:
+            titile = sb1.get_title()
+            print(f'title is : {titile}')
+            continue_buttons = driver.find_elements(By.CSS_SELECTOR, 'h1.sc-bdnylx')
+            for button in continue_buttons:
+                if 'Sign in to CodeSandbox' in button.text:
+                    print(f"H1 found: {button.text}, clicking...")
+                    codesandbox_login(CSB_id)
+            
+            if 'Recent - CodeSandbox' in titile:
+                return True
+
+        except Exception as e:
+            print(f' are_codesand_logged ERROR:{e}')
+
+sb1 = Driver(uc=False, headed= True,  user_data_dir=chrome_user_data_dir, binary_location=chrome_binary_path)
+sb1.maximize_window()
+url = "chrome://extensions/"
+sb1.open(url)
+print(sb1.get_title())
+fresh = are_extensions_exist()
+if fresh:
+        mysterium = install_extensions('mysterium')
+        nopecha = install_extensions('nopecha')
+        cookie = install_extensions('cookie')
+        fingerprint = install_extensions('fingerprint')
+        mfhelper = install_extensions('mfhelper')
+        if fingerprint and mysterium and nopecha and cookie and mfhelper:
+            print('All Extensions are installed..')
+            if pin_extensions():
+                print('All Extensions are pinned')
+
+codesandlogged = are_codesand_logged(sb1)
+print(codesandlogged)

e67beeae53441134c67c1a042d91c485106da503
mcnutthelen8 <mcnutthelen8@gmail.com>
2025-03-09 11:15:54 +0530

    Delete mfhelper/popup.js

--- mfhelper/popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("startClicker").addEventListener("click", () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        function: clickPlayButton
-      });
-    });
-  });
-  

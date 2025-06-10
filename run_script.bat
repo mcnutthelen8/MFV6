79148992b89928dd73bcf11dc2919ec7cad2d613
mcnutthelen8 <mcnutthelen8@gmail.com>
2025-06-10 10:49:19 +0530

    Update run_script.bat

--- run_script.bat
+++ run_script.bat
@@ -2,7 +2,7 @@
 
 @echo off
 :loop
-cd C:\Users\WhatUpTime.com\Downloads\MFV6-main\MFV6-main
+cd C:\Users\Administrator\Downloads\MFV6-main\MFV6-main
 echo Killing python and chrome processes...
 taskkill /f /im python.exe >nul 2>&1
 taskkill /f /im chrome.exe >nul 2>&1

4dbb55900ce48494034c8e560e4cad5945d7d97a
mcnutthelen8 <mcnutthelen8@gmail.com>
2026-03-22 16:21:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from humancursor import WebCursor
+
 import json
 
 
